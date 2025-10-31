fef03c5e8ee066f01661fdf9e4ac9d892f242cce
rashidflutterDev <rashidFlutterDeveloper@gmail.com>
2025-10-31 17:28:52 +0500

    nav for desktop screen done

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
@@ -43,15 +44,24 @@ function Navbar() {
   {/* on hover it will show the dropdown of profile , login etc */}
 
   <div className=' group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
 
-    
+      <p className='cursor-pointer hover:text-black'>My Profile</p>
+       <p className='cursor-pointer hover:text-black'>Orders</p>
+      <p className='cursor-pointer hover:text-black'>Logout</p>
+    </div>
   </div>
+
+  {/* dropdown end */}
+
+
     </div>
 
-        <div className='relative'>
-          <img src={assets.cart_icon} alt="cart" className='w-5' />
-          <div className='absolute -top-2 -right-3 bg-orange-500 text-white text-xs w-4 h-4 rounded-full flex items-center justify-center'>3</div>
-        </div>
+         <Link to="/cart" className='relative'>
+          <img src={assets.cart_icon} alt="cart" className='w-5 min-w-5' />
+          <p className='absolute  right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>3</p>
+              </Link>
+
 
       </div>
 
